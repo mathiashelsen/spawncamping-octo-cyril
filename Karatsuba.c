de9bdc6cd53b6d5256b7eb758dfbc2dbfd717f7e
Mathias Helsen <helsen.mathias@gmail.com>
2015-01-23 08:33:34 +0100

    working out the bugs

--- Karatsuba.c
+++ Karatsuba.c
@@ -18,8 +18,10 @@ int main(void)
 {
     uint32_t x[2] = {10, 20};
     uint32_t y[2] = {20, 10};
+    uint32_t *xy;
     accumulate(x, y, 0, 2);
-    printf("%d %d\n", x[0], x[1]);
+    multiply(&xy, x, y, 0, 2);
+    printf("%d %d %d %d\n", xy[0], xy[1], xy[2], xy[3]);
 
     return 0;
 }
@@ -44,14 +46,19 @@ int add(uint32_t **dest, uint32_t *x, int offsetX, int lengthX, uint32_t *y, int
     //Prepare destination memory
     int maxLength = (lengthX > lengthY) ? lengthX : lengthY;
     *dest = malloc(sizeof(uint32_t)*(maxLength + 1));
-    memset(dest, 0, sizeof(uint32_t)*(maxLength + 1));
+    memset(*dest, 0, sizeof(uint32_t)*(maxLength + 1));
 
     // Add the two numbers by calling accumulate
     accumulate( *dest, x, offsetX, lengthX );
     accumulate( *dest, y, offsetY, lengthY );
+    int i = 0;
+    while( (dest[i] != 0) && (i < (maxLength+1)) )
+    {
+	i++;
+    }
 
     // Done and done
-    return 0;
+    return i;
 }
 
 int decrease(uint32_t *dest, uint32_t *x, int offset, int length)
@@ -70,32 +77,49 @@ int decrease(uint32_t *dest, uint32_t *x, int offset, int length)
 int multiply(uint32_t **dest, uint32_t *x, uint32_t *y, int start, int length)
 {
     *dest = malloc(sizeof(uint32_t)*2*length); 
-    memset(dest, 0, sizeof(uint32_t)*2*length);
+    memset(*dest, 0, sizeof(uint32_t)*2*length);
 
     if((length) == 1)
     {
 	uint64_t tmp = (uint64_t)x[start]*(uint64_t)y[start];
 	(*dest)[start] = (uint32_t) (tmp & 0xffffffff);
 	(*dest)[start+1] = (uint32_t) (tmp >> 32);
+	if( (*dest)[start+1] == 0 )
+	    return 1;
+	else
+	    return 2;
     }
     else
     {
 	int lengthL = length/2;
 	int lengthH = length - lengthL;
+	printf("%d %d %d", length, lengthL, lengthH);
 	// Multiply A and C
-	uint32_t *ac, *bd, *apb, *cpd, *ip, *jp;
+	uint32_t *ac, *bd, *apb, *cpd, *ip;
 
-	multiply(&ac, x, y, start, lengthL);		// A*C
-	multiply(&bd, x, y, start+lengthL, lengthH);	// B*D
-	add(&apb, x, start, lengthL, x, start+lengthL, lengthH);    // APB = A + B 
-	add(&cpd, y, start, lengthL, y, start+lengthL, lengthH);    // CPD = C + D
-	multiply(&ip, apb, cpd, 0, lengthH+1);		// IP = APB*CPD
-	decrease(ip, ac, 0, lengthL*2);			// IP = IP - AC 
-	decrease(ip, bd, 0, lengthH*2);			// IP = IP - BD
+	int lengthAC = multiply(&ac, x, y, start, lengthL);		// A*C
+	int lengthBD = multiply(&bd, x, y, start+lengthL, lengthH);	// B*D
+	int lengthAPB = add(&apb, x, start, lengthL, x, start+lengthL, lengthH);    // APB = A + B 
+	int lengthCPD = add(&cpd, y, start, lengthL, y, start+lengthL, lengthH);    // CPD = C + D
+	printf("\t%d %d %d %d\n", lengthAC, lengthBD, lengthAPB, lengthCPD);
 
-	accumulate(*dest, ac, start, lengthL*2);
-	accumulate(*dest, ip, start+lengthL, lengthH*2);
-	accumulate(*dest, bd, start+length, lengthH*2);
+	int maxLength = (lengthAPB > lengthCPD) ? lengthAPB : lengthCPD;
+	int lengthIP = multiply(&ip, apb, cpd, 0, maxLength);		// IP = APB*CPD
+
+	decrease(ip, ac, 0, lengthAC);			// IP = IP - AC 
+	decrease(ip, bd, 0, lengthBD);			// IP = IP - BD
+
+	accumulate(*dest, ac, start, lengthAC);
+	accumulate(*dest, ip, start+lengthL, lengthIP);
+	accumulate(*dest, bd, start+length, lengthBD);
+
+	free(ac); free(bd); free(apb); free(cpd); free(ip);
+
+	int i = 0;
+	while( ((*dest)[start + i] != 0) && (i < lengthH*2) )
+	{
+	    i++;
+	}
+	return i;
     }
-    return 0;
 }
